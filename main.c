bbbf0cc60f19b06d68b6cae7c52bd2d8fc8c256d
Koustab Saikia <Koustavsaikia63@gmail.com>
2025-01-19 11:28:21 +0530

    Basic Input of Marks

--- main.c
+++ main.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
+void calcPercentage();
+
 int main(){
+    int s1, s2, s3, s4, s5;
+    printf("Enter marks in 1st subject : ");
+    scanf("%d", &s1);
+    printf("Enter marks in 2nd subject : ");
+    scanf("%d", &s2);
+    printf("Enter marks in 3rd subject : ");
+    scanf("%d", &s3);
+    printf("Enter marks in 4th subject : ");
+    scanf("%d", &s4);
+    printf("Enter marks in 5th subject : ");
+    scanf("%d", &s5);
     return 0;
 }
